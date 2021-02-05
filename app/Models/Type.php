ff20cc556036f55c64bea10a3b4fbef3b36fecd3
lafcha <charlotte.bronsard@gmail.com>
2021-02-05 21:26:12 +0100

    ajout des Models Brand & Type

---
+++ app/Models/Type.php
@@ -0,0 +1,104 @@
+<?php
+
+class Type {
+
+    /*********** Propriétés ************/
+
+    private $id;
+    private $name;
+    private $footer_order;
+    private $created_at;
+    private $updated_at;
+
+     /*********** Méthodes ************/
+
+      /*********** Getters & Setters ************/
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of name
+     */ 
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */ 
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of footer_order
+     */ 
+    public function getFooter_order()
+    {
+        return $this->footer_order;
+    }
+
+    /**
+     * Set the value of footer_order
+     *
+     * @return  self
+     */ 
+    public function setFooter_order($footer_order)
+    {
+        $this->footer_order = $footer_order;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_at
+     */ 
+    public function getCreated_at()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set the value of created_at
+     *
+     * @return  self
+     */ 
+    public function setCreated_at($created_at)
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updated_at
+     */ 
+    public function getUpdated_at()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set the value of updated_at
+     *
+     * @return  self
+     */ 
+    public function setUpdated_at($updated_at)
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+}
